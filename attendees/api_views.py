cb10aa30c20aca542da7f58c80853b2b0286e63b
Chris <c.r.zambito@gmail.com>
2023-03-22 20:41:14 -0400

    bug fixes

--- attendees/api_views.py
+++ attendees/api_views.py
@@ -1,7 +1,10 @@
 from django.http import JsonResponse
 from common.json import ModelEncoder
 from events.api_views import ConferenceListEncoder
+from events.models import Conference
 from .models import Attendee
+from django.views.decorators.http import require_http_methods
+import json
 
 
 class ShowAttendeeListEncoder(ModelEncoder):
@@ -11,12 +14,30 @@ class ShowAttendeeListEncoder(ModelEncoder):
     ]
 
 
+@require_http_methods(["GET", "POST"])
 def api_list_attendees(request, conference_id):
-    attendee = Attendee.objects.filter(conference=conference_id)
-    return JsonResponse(
-        {"attendee": attendee},
-        encoder=ShowAttendeeListEncoder
-    )
+    if request.method == "GET":
+        attendee = Attendee.objects.filter(conference=conference_id)
+        return JsonResponse(
+            {"attendee": attendee},
+            encoder=ShowAttendeeListEncoder
+        )
+    else:
+        content = json.loads(request.body)
+        try:
+            conference = Conference.objects.get(id=conference_id)
+            content['conference'] = conference
+        except Conference.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid conference id"},
+                status=400,
+            )
+        attendee = Attendee.objects.create(**content)
+        return JsonResponse(
+            attendee,
+            encoder=ShowAttendeeListEncoder,
+            safe=False,
+        )
 
 
 class ShowAttendeeEncoder(ModelEncoder):
@@ -32,10 +53,26 @@ class ShowAttendeeEncoder(ModelEncoder):
     }
 
 
+@require_http_methods(["DELETE", "GET", "PUT"])
 def api_show_attendee(request, id):
-    attendee = Attendee.objects.get(id=id)
-    return JsonResponse(
-        attendee,
-        encoder=ShowAttendeeEncoder,
-        safe=False,
-    )
+    if request.method == 'GET':
+        attendee = Attendee.objects.get(id=id)
+        return JsonResponse(
+            attendee,
+            encoder=ShowAttendeeEncoder,
+            safe=False,
+        )
+    elif request.method == "DELETE":
+        count, _ = Attendee.objects.filter(id=id).delete()
+        return JsonResponse(
+            {"deleted": count > 0}
+            )
+    else:
+        content = json.loads(request.body)
+        Attendee.objects.filter(id=id).update(**content)
+        attendee = Attendee.objects.get(id=id)
+        return JsonResponse(
+            attendee,
+            encoder=ShowAttendeeEncoder,
+            safe=False,
+        )
